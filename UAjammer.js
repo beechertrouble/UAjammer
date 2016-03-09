d0a486eea6c93b354f566a021dec9de1432a4d7b
beecher <beechertrouble@gmail.com>
2016-03-09 11:21:30 -0500

    windows jams

--- UAjammer.js
+++ UAjammer.js
@@ -151,7 +151,7 @@
 					
 				case(UA.Raw.match(/windows/i) && UA.Raw.match(/windows/i).length > 0):
 					UA.OS = "windows";
-					start = UA.Raw.match(/windows/i) + 8;
+					start = UA.Raw.indexOf(UA.Raw.match(/windows/i)) + 8;
 					console.log('start : ', start);
 					end = UA.Raw.indexOf(";", start) - start;
 					console.log('end : ', end);
